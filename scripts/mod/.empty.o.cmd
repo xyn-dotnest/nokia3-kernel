32a83be32b1267cf53069d605c7cbf7880cc3286
Xyn Dotnest <xyn@localhost.localdomain>
2020-09-09 11:13:10 +0300

    test

---
+++ scripts/mod/.empty.o.cmd
@@ -0,0 +1,9 @@
+cmd_scripts/mod/empty.o := /home/xyn/nokia3/aarch64-linux-android-4.9/bin/aarch64-linux-android-gcc -Wp,-MD,scripts/mod/.empty.o.d  -nostdinc -isystem /home/xyn/nokia3/aarch64-linux-android-4.9/bin/../lib/gcc/aarch64-linux-android/4.9.x/include -I./arch/arm64/include -Iarch/arm64/include/generated  -Iinclude -I./arch/arm64/include/uapi -Iarch/arm64/include/generated/uapi -I./include/uapi -Iinclude/generated/uapi -include ./include/linux/kconfig.h  -D__KERNEL__ -mlittle-endian -Wall -Wundef -Wstrict-prototypes -Wno-trigraphs -fno-strict-aliasing -fno-common -Werror-implicit-function-declaration -Wno-format-security -std=gnu89 -mgeneral-regs-only -fno-pic -fstack-usage -fno-delete-null-pointer-checks -fno-PIE -Os -Wno-maybe-uninitialized --param=allow-store-data-races=0 -Werror=frame-larger-than=1 -Wframe-larger-than=1700 -fstack-protector-strong -Wno-unused-but-set-variable -fno-omit-frame-pointer -fno-optimize-sibling-calls -fno-var-tracking-assignments -g -Wdeclaration-after-statement -Wno-pointer-sign -fno-strict-overflow -fno-stack-check -fconserve-stack -Werror=implicit-int -Werror=strict-prototypes -Werror=date-time    -D"KBUILD_STR(s)=\#s" -D"KBUILD_BASENAME=KBUILD_STR(empty)"  -D"KBUILD_MODNAME=KBUILD_STR(empty)" -c -o scripts/mod/.tmp_empty.o scripts/mod/empty.c
+
+source_scripts/mod/empty.o := scripts/mod/empty.c
+
+deps_scripts/mod/empty.o := \
+
+scripts/mod/empty.o: $(deps_scripts/mod/empty.o)
+
+$(deps_scripts/mod/empty.o):
